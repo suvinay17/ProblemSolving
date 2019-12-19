23b437362c889fdcb5f6367a3d9a2c7e9beb76fc
Suvinay Bothra <suvinay@Suvinays-MacBook-Pro.local>
2019-12-19 00:26:16 -0500

    Two Sum Code

---
+++ twoSum.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target-nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
